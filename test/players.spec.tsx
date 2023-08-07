2e2d0eceba847974a6d0296c95fb2fc74a5b972d
Olaf Merkert <olaf.merkert@tngtech.com>
2023-08-07 13:54:26 +0200

    move entering player count into helper function

--- test/players.spec.tsx
+++ test/players.spec.tsx
@@ -1,31 +1,46 @@
 import { cleanup, render } from "@solidjs/testing-library";
 import userEvent from "@testing-library/user-event";
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test } from "vitest";
 import App from "../src/App";
 
-describe("selecting the number of players", () => {
-  let user: ReturnType<typeof userEvent.setup>;
-  beforeEach(() => {
-    user = userEvent.setup();
-  });
+const renderApp = () => {
+  const user = userEvent.setup();
+  const app = render(() => <App />);
 
-  afterEach(cleanup);
-
-  const renderApp = () => {
-    const app = render(() => <App />);
-
-    return {
-      getPlayerCountInput(): HTMLInputElement {
-        return app.getByLabelText("Player Count");
-      },
-      findPlayerColumns(): Promise<HTMLDivElement[]> {
-        return app.findAllByTestId("player-column");
-      },
-      getPlusButton(): HTMLButtonElement {
-        return app.getByText("+");
-      },
-    };
+  const getPlayerCountInput = (): HTMLInputElement => {
+    return app.getByLabelText("Player Count");
   };
+
+  const getPlusButton = (): HTMLButtonElement => {
+    return app.getByText("+");
+  };
+
+  const clickPlusButton = async () => {
+    await user.click(getPlusButton());
+  };
+
+  const enterPlayerCount = async (playerNumber: string) => {
+    const playerCountInput = getPlayerCountInput();
+
+    await user.clear(playerCountInput);
+    await user.type(playerCountInput, playerNumber);
+    await user.tab();
+  };
+
+  const findPlayerColumns = (): Promise<HTMLDivElement[]> => {
+    return app.findAllByTestId("player-column");
+  };
+  return {
+    getPlayerCountInput,
+    getPlusButton,
+    clickPlusButton,
+    enterPlayerCount,
+    findPlayerColumns,
+  };
+};
+
+describe("selecting the number of players", () => {
+  afterEach(cleanup);
 
   test("the default is a two player game", async () => {
     const { getPlayerCountInput, findPlayerColumns } = renderApp();
@@ -34,34 +49,28 @@ describe("selecting the number of players", () => {
   });
 
   test("increases the player count when the plus button is clicked", async () => {
-    const { getPlusButton, getPlayerCountInput, findPlayerColumns } =
+    const { clickPlusButton, getPlayerCountInput, findPlayerColumns } =
       renderApp();
-    await user.click(getPlusButton());
+    await clickPlusButton();
     expect(getPlayerCountInput().value).toBe("3");
     expect(await findPlayerColumns()).toHaveLength(3);
   });
 
   test("updates the player count when a new number is entered", async () => {
-    const { getPlayerCountInput, findPlayerColumns } = renderApp();
-    const playerCountInput = getPlayerCountInput();
+    const { enterPlayerCount, getPlayerCountInput, findPlayerColumns } =
+      renderApp();
+    await enterPlayerCount("5");
 
-    await user.clear(playerCountInput);
-    await user.type(playerCountInput, "5");
-    await user.tab();
-
-    expect(playerCountInput.value).toBe("5");
+    expect(getPlayerCountInput().value).toBe("5");
     expect(await findPlayerColumns()).toHaveLength(5);
   });
 
   test("prevents a negative player count by taking the absolute value", async () => {
-    const { getPlayerCountInput, findPlayerColumns } = renderApp();
-    const playerCountInput = getPlayerCountInput();
+    const { enterPlayerCount, getPlayerCountInput, findPlayerColumns } =
+      renderApp();
+    await enterPlayerCount("-4");
 
-    await user.clear(playerCountInput);
-    await user.type(playerCountInput, "-4");
-    await user.tab();
-
-    expect(playerCountInput.value).toBe("4");
+    expect(getPlayerCountInput().value).toBe("4");
     expect(await findPlayerColumns()).toHaveLength(4);
   });
 });
