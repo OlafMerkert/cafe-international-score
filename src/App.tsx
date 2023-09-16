5e3d538173ee013fa097ed81bb540c6c1696ab27
Olaf Merkert <olaf.merkert@tngtech.com>
2023-09-16 21:49:58 +0200

    move app content into center of the screen

--- src/App.tsx
+++ src/App.tsx
@@ -28,44 +28,48 @@ const App: Component = () => {
   const [showScore, setShowScore] = createSignal(false);
 
   return (
-    <div>
-      <header>
-        <h1 class="text-xl font-bold px-8">Cafe International Score Keeping</h1>
-      </header>
+    <div class="flex flex-col items-center">
+      <div class="flex-initial">
+        <header>
+          <h1 class="text-xl font-bold px-8">
+            Cafe International Score Keeping
+          </h1>
+        </header>
 
-      <PlayerCount count={playerCount} setCount={setPlayerCount} />
+        <PlayerCount count={playerCount} setCount={setPlayerCount} />
 
-      <ShowScoreSetting showScore={showScore} setShowScore={setShowScore} />
+        <ShowScoreSetting showScore={showScore} setShowScore={setShowScore} />
 
-      <div class="flex flex-row">
-        <For each={store.players}>
-          {(player, playerIndex) => {
-            return (
-              <div
-                class="m-1 border border-blue-800 p-2"
-                data-testid="player-column"
-              >
-                <PlayerName
-                  playerIndex={playerIndex()}
-                  playerName={() => player.playerName}
-                  setPlayerName={(name) => setPlayerName(playerIndex(), name)}
-                />
+        <div class="flex flex-row">
+          <For each={store.players}>
+            {(player, playerIndex) => {
+              return (
+                <div
+                  class="m-1 border border-blue-800 p-2"
+                  data-testid="player-column"
+                >
+                  <PlayerName
+                    playerIndex={playerIndex()}
+                    playerName={() => player.playerName}
+                    setPlayerName={(name) => setPlayerName(playerIndex(), name)}
+                  />
 
-                <ScoreColumn
-                  showScore={showScore}
-                  playerIndex={playerIndex()}
-                  playerCount={playerCount}
-                  allScores={() => player.playerScore}
-                  addScore={(score) => addScore(playerIndex(), score)}
-                  removeLastScore={() => removeLastScore(playerIndex())}
-                />
-              </div>
-            );
-          }}
-        </For>
+                  <ScoreColumn
+                    showScore={showScore}
+                    playerIndex={playerIndex()}
+                    playerCount={playerCount}
+                    allScores={() => player.playerScore}
+                    addScore={(score) => addScore(playerIndex(), score)}
+                    removeLastScore={() => removeLastScore(playerIndex())}
+                  />
+                </div>
+              );
+            }}
+          </For>
+        </div>
+
+        <ResetGame resetScores={resetScores} />
       </div>
-
-      <ResetGame resetScores={resetScores} />
     </div>
   );
 };
